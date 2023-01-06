a73f067a929be7c57720c8945110d16283dade9c
Ares <ares.r@mblhightech.com>
2023-01-06 16:58:03 +0200

    Updated script

--- utils/bucket.ts
+++ utils/bucket.ts
@@ -1,14 +1,14 @@
 export function notFound() {
-  return new Response("Not found", { status: 404 });
+  return new Response('Not found', { status: 404 })
 }
 
 export function parseBucketPath(context): [any, string] {
-  const { request, env, params } = context;
-  const url = new URL(request.url);
+  const { request, env, params } = context
+  const url = new URL(request.url)
 
-  const pathSegments = (params.path || []) as String[];
-  const path = decodeURIComponent(pathSegments.join("/"));
-  const driveid = url.hostname.replace(/\..*/, "");
+  const pathSegments = (params.path || []) as String[]
+  const path = decodeURIComponent(pathSegments.join('/'))
+  const driveid = url.hostname.replace(/\..*/, '')
 
-  return [env[driveid] || env["BUCKET"], path];
+  return [env[driveid] || env['BUCKET'], path]
 }
